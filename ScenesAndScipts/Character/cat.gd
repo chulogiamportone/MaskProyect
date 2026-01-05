d7b784fb812d5a0f5f6e9b782defe3908ebefadc
chulogiamportone <chulogiamportone@gmail.com>
2026-01-05 01:15:00 -0300

    cat

---
+++ ScenesAndScipts/Character/cat.gd
@@ -0,0 +1,6 @@
+extends Node3D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	animation_player.play("Take 001")
